e1ae23170b115d01c0e0925a3cf30b0b1607bdaf
pauldavies83 <pauldavies83@googlemail.com>
2017-08-31 08:37:03 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+From GDG Liverpool, 31st August 2017.
+https://www.meetup.com/GDG-Liverpool/events/240206929/
+
+An attempt at the following Kata
+http://www.butunclebob.com/ArticleS.UncleBob.TheBowlingGameKata
+
+trying to stick to "the rules"
+http://butunclebob.com/ArticleS.UncleBob.TheThreeRulesOfTdd
---
+++ app/src/test/java/com/example/pauldavies/tddbowling/SampleTest.kt
@@ -0,0 +1,15 @@
+package com.example.pauldavies.tddbowling
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import com.natpryce.hamkrest.throws
+
+import org.junit.jupiter.api.Test
+
+class SampleTest {
+
+    @Test
+    fun nothing() {
+        assertThat(true, equalTo(true))
+    }
+}
